c09e901026cebc588dabadd032a075b8f797b955
Codeodata <114332878+Codeodata@users.noreply.github.com>
2025-02-04 07:22:44 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "mundose22"
+    bucket                 = "bucket-terraform-lab"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
